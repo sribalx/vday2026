2dd5d84a3e4cd9b8179d99b121a8c5ddc90231da
sribalx <sricharanbala@yahoo.com.sg>
2026-02-08 21:01:14 -0800

    last

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -124,7 +124,7 @@ export default function Home() {
               Will you be my Valentine?
             </h1>
             
-            <div className="w-full max-w-3xl bg-white border-4 border-black shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] flex items-center justify-center overflow-hidden mb-8 relative h-[100px] md:h-[200px] lg:h-[250px]">
+            <div className="w-full max-w-3xl bg-white border-4 border-black shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] flex items-center justify-center overflow-hidden mb-8 relative h-[100px] md:h-[100px] lg:h-[180px]">
               <img 
                   src="/ustgt.jpg"
                   alt="photo of us" 
